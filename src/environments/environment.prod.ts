21cff0802998777ab8af23621a15f8fcba2b77de
Sergey Bolzan <siarhei.bolzan@netcracker.com>
2019-03-01 18:15:41 +0300

    change build for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAe5zBz3OKXI5PDWGdg3J4sKpzgUO1QXAw',
+    authDomain: 'circleci-firebase.firebaseapp.com',
+    databaseURL: 'https://circleci-firebase.firebaseio.com',
+    projectId: 'circleci-firebase',
+    storageBucket: 'circleci-firebase.appspot.com',
+    messagingSenderId: '541569099484'
+  }
 };
